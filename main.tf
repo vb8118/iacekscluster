de73514da96f47afa058d266fcf61376b44ee311
The amanecer <vbmac@proton.me>
2024-03-24 08:34:23 +0530

    added variables

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
-provider "aws" {
-  region = "us-east-1"
-}
+
 
 # resource "aws_instance" "my_vm" {
 #   ami           = "ami-0e731c8a588258d0d" //Ubuntu AMI
---
+++ output.tf
@@ -0,0 +1,13 @@
+# output "eks_iam_app_role" {
+#   value = aws_iam_role.eks-app-WebIdentity.name
+# }
+# output "eks_cluster_name" {
+#   value = aws_eks_cluster.eks-cluster.name
+# }
+# output "aws_eks_cluster_eks_cluster_arn" {
+#   value = aws_eks_cluster.eks-cluster.arn
+# }
+
+output "aws_region" {
+  value = aws_region
+}
