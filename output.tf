c1ae183ebb88ae83f7de3c8e3ca61b31b4fceb44
The amanecer <vbmac@proton.me>
2024-03-24 10:00:36 +0530

    cleaned variables and create vpc

--- output.tf
+++ output.tf
@@ -1,13 +1,3 @@
-# output "eks_iam_app_role" {
-#   value = aws_iam_role.eks-app-WebIdentity.name
-# }
-# output "eks_cluster_name" {
-#   value = aws_eks_cluster.eks-cluster.name
-# }
-# output "aws_eks_cluster_eks_cluster_arn" {
-#   value = aws_eks_cluster.eks-cluster.arn
-# }
-
-output "aws_region" {
-  value = aws_region
+output "aws_vpc_id" {
+    value = aws_vpc.main.id
 }
