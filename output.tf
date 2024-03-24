ec688559c865659c9c4f9e3982ad940516b7f7c1
The amanecer <vbmac@proton.me>
2024-03-24 11:09:00 +0530

    added subnets

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "aws_vpc_id" {
     value = aws_vpc.main.id
 }
+
+output "aws_subnets_private" {
+  value = aws_subnet.private.cidr_block
+}
+
+output "aws_subnets_public" {
+  value = aws_subnet.public.cidr_block
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
 variable "iaceksregion" {
     default = "us-east-1"
 }
+
+variable "vpc_cidr_block" {
+    default = "10.55.55.0/24"
+}
+
+variable "subnet_public" {
+    default = "10.55.55.0/25"
+}
+
+variable "subnet_private" {
+    default = "10.55.55.128/25"
+}
