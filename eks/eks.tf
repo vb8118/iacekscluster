0e8608efce990ccba0694fc3b58891e52282d4fb
The amanecer <vbmac@proton.me>
2024-03-31 18:23:12 +0530

    removed version

---
+++ eks/eks.tf
@@ -0,0 +1,38 @@
+
+# Resource: aws_eks_cluster
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_cluster
+
+resource "aws_eks_cluster" "eks" {
+
+  # Name of the cluster.
+  name = var.clusterName
+
+  # The Amazon Resource Name (ARN) of the IAM role that provides permissions for 
+  # the Kubernetes control plane to make calls to AWS API operations on your behalf
+  role_arn = aws_iam_role.eks_cluster.arn
+
+#   # Desired Kubernetes master version
+#   version = "1.18"
+
+  vpc_config {
+    # Indicates whether or not the Amazon EKS private API server endpoint is enabled
+    endpoint_private_access = false
+
+    # Indicates whether or not the Amazon EKS public API server endpoint is enabled
+    endpoint_public_access = true
+
+    # Must be in at least two different availability zones
+    subnet_ids = [
+      aws_subnet.azp_private.id,
+      aws_subnet.azs_private.id,
+      aws_subnet.azp_public.id,
+      aws_subnet.azs_private.id
+    ]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.amazon_eks_cluster_policy
+  ]
+}
